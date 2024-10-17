19f4b58fdf06a1f5faa58e31ea36b4636dce8286
alaizajean06 <alaizajeanbacarro06@gmail.com>
2024-10-18 03:10:38 +0800

    Initial commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <ion-page>
+        <ion-header>
+         <ion-Toolbar>
+         <ion-title>Home Screen</ion-title>
+         </ion-Toolbar>
+        </ion-header>
+        <ion-content>
+          
+        </ion-content>
+    </ion-page>
+</template>
+<script lang="ts">
+
+import { defineComponent, DefineComponent } from 'vue';
+import { IonToolbar, IonTitle, IonHeader, IonPage, IonContent } from '@ionic/vue';
+
+export default defineComponent({
+    name: "IndexScreen",
+    components: { IonToolbar, IonTitle, IonHeader, IonPage, IonContent },
+    methods: {
+        async nextScreen() {
+            this.$router.push('/signin');
+        }
+    }
+});
+</script>
---
+++ src/views/signin/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <ion-page>
+        <ion-header>
+         <ion-Toolbar>
+            <ion-buttons slot="start">
+                <ion-back-button></ion-back-button>
+            </ion-buttons>
+         <ion-title>Sign in Page</ion-title>
+         </ion-Toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <div>
+  <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+</div>
+        </ion-content>
+        <ion-footer>
+         <ion-Toolbar>
+            <button slot="end" class="btn btn-primary w-45">sign in</button>
+         </ion-Toolbar>
+        </ion-footer>
+    </ion-page>
+</template>
+<script lang="ts">
+
+import { defineComponent, DefineComponent } from 'vue';
+import { IonBackButton, IonButton, IonToolbar, IonTitle, IonHeader, IonPage, IonContent, IonFooter } from '@ionic/vue';
+import { add } from 'ionicons/icons';
+
+export default defineComponent({
+    name: "IndexScreen",
+    components: { IonBackButton, IonButton, IonToolbar, IonTitle, IonHeader, IonPage, IonContent, IonFooter },
+});
+</script>
